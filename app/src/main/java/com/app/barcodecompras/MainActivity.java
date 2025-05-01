8800a97312eac9c01dbe0c6ca2d9baac30087963
NicoVitoriano001 <nicovitoriano@gmail.com>
2025-05-01 12:22:58 -0300

    Primeiro Commit

--- app/src/main/java/com/app/barcodecompras/MainActivity.java
+++ app/src/main/java/com/app/barcodecompras/MainActivity.java
@@ -2,33 +2,16 @@ package com.app.barcodecompras;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
-
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import java.io.File;
-
-import com.app.barcodecompras.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
